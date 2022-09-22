09f969389365edf784c642e86c99c7455d8b7bf0
mwlk <mirkoivowlk@gmail.com>
2022-09-22 00:52:07 -0300

    coverage

--- second/testing/main.go
+++ second/testing/main.go
@@ -10,3 +10,10 @@ func getMax(x, y int) int {
 	}
 	return y
 }
+
+func fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
--- second/testing/main_test.go
+++ second/testing/main_test.go
@@ -27,3 +27,42 @@ func TestSum(t *testing.T) {
 		}
 	}
 }
+
+func TestMax(t *testing.T) {
+	tables := []struct {
+		a int
+		b int
+		n int
+	}{
+		{4, 2, 4},
+		{3, 2, 3},
+		{2, 5, 5},
+	}
+
+	for _, item := range tables {
+		max := getMax(item.a, item.b)
+
+		if max != item.n {
+			t.Errorf("max expected %d but result %d", max, item.n)
+		}
+	}
+}
+
+func TestFibonacci(t *testing.T) {
+	table := []struct {
+		a int
+		n int
+	}{
+		{1, 1},
+		{8, 21},
+		{50, 12586269025},
+	}
+
+	for _, item := range table {
+		fib := fibonacci(item.a)
+
+		if fib != item.n {
+			t.Errorf("expected %d but result is %d", fib, item.n)
+		}
+	}
+}
